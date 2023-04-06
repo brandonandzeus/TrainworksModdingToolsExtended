94e49617a4c8e9642d6f42b6c307e73d19b9931b
Brandon <brandon@brandonandzeus.com>
2023-04-06 14:39:25 -0700

    Add RelicDataBuilders with new fields introduced from the DLC.
    Alias hack for more unchanged databuilders.

---
+++ TrainworksModdingToolsExtended/Builders/RelicEffectConditionBuilder.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+// Changes: Capitialized properties to keep consistency across the framework.
+namespace Trainworks.BuildersV2
+{
+	public class RelicEffectConditionBuilder
+	{
+		public CardStatistics.TrackedValueType ParamTrackedValue { get; set; }
+
+		public CardStatistics.CardTypeTarget ParamCardType { get; set; }
+
+		/// <summary>
+		/// Determines if we should use the total trigger count as the tracked value instead of using a value from CardStatistics. Use this to limit triggers to X per turn or per battle.
+		/// </summary>
+		public bool ParamTrackTriggerCount { get; set; }
+
+		public CardStatistics.EntryDuration ParamEntryDuration { get; set; }
+
+		public RelicEffectCondition.Comparator ParamComparator { get; set; } = RelicEffectCondition.Comparator.Equal | RelicEffectCondition.Comparator.GreaterThan;
+
+		public int ParamInt { get; set; }
+
+		/// <summary>
+		/// Allow multiple triggers per duration that is defined as Entry Duration. This can be used to limit a relic from trigger more than X times per turn or per battle 
+		/// </summary>
+		public bool AllowMultipleTriggersPerDuration { get; set; } = true;
+
+		public string ParamSubtype { get; set; }
+
+		/// <summary>
+		/// Builds the RelicEffectCondition represented by this builder's parameters
+		/// </summary>
+		/// <returns>The newly created RelicEffectCondition</returns>
+		public RelicEffectCondition Build()
+		{
+			RelicEffectCondition relicEffectCondition = new RelicEffectCondition();
+
+			AccessTools.Field(typeof(RelicEffectCondition), "paramTrackedValue").SetValue(relicEffectCondition, ParamTrackedValue);
+			AccessTools.Field(typeof(RelicEffectCondition), "paramCardType").SetValue(relicEffectCondition, ParamCardType);
+			AccessTools.Field(typeof(RelicEffectCondition), "paramTrackTriggerCount").SetValue(relicEffectCondition, ParamTrackTriggerCount);
+			AccessTools.Field(typeof(RelicEffectCondition), "paramEntryDuration").SetValue(relicEffectCondition, ParamEntryDuration);
+			AccessTools.Field(typeof(RelicEffectCondition), "paramComparator").SetValue(relicEffectCondition, ParamComparator);
+			AccessTools.Field(typeof(RelicEffectCondition), "paramInt").SetValue(relicEffectCondition, ParamInt);
+			AccessTools.Field(typeof(RelicEffectCondition), "allowMultipleTriggersPerDuration").SetValue(relicEffectCondition, AllowMultipleTriggersPerDuration);
+			AccessTools.Field(typeof(RelicEffectCondition), "paramSubtype").SetValue(relicEffectCondition, ParamSubtype);
+
+			return relicEffectCondition;
+		}
+	}
+}
--- TrainworksModdingToolsExtended/Builders/UnchangedBuilders.cs
+++ TrainworksModdingToolsExtended/Builders/UnchangedBuilders.cs
@@ -6,11 +6,8 @@ namespace Trainworks.BuildersV2
 {
     // A hack for class name aliasing, nothing changed here in the DLC, but wanted to make
     // usuability easier with a clean switch from Trainworks.Builders to Trainworks.BuildersV2.
-    class ChampionCardDataBuilder : Builders.ChampionCardDataBuilder
-    {
-    }
-
-    class CardPoolBuilder : Builders.CardPoolBuilder
-    {
-    }
+    class ChampionCardDataBuilder : Builders.ChampionCardDataBuilder { }
+    class CardUpgradeTreeDataBuilder : Builders.CardUpgradeTreeDataBuilder { }
+    class CardPoolBuilder : Builders.CardPoolBuilder { }
+    class EnhancerDataBuilder : Builders.EnhancerDataBuilder { }
 }
