56140461e0fde0b88f80b3c376767418daf5d1bb
Brandon <brandon@brandonandzeus.com>
2023-04-06 01:13:59 -0700

    Added SubtypeDataBuilder and a few unchanged builders.

---
+++ TrainworksModdingToolsExtended/Builders/UnchangedBuilders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainworks.BuildersV2
+{
+    // A hack for class name aliasing, nothing changed here in the DLC, but wanted to make
+    // usuability easier with a clean switch from Trainworks.Builders to Trainworks.BuildersV2.
+    class ChampionCardDataBuilder : Builders.ChampionCardDataBuilder
+    {
+    }
+
+    class CardPoolBuilder : Builders.CardPoolBuilder
+    {
+    }
+}
