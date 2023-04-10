b73cff7136544b98aecb64d40f8de6657fcd98d0
Brandon <brandon@brandonandzeus.com>
2023-04-10 01:00:49 -0700

    Add ability to modify or add ExpertChallenges.

---
+++ TrainworksModdingToolsExtended/Builders/SpChallengeDataBuilder.cs
@@ -0,0 +1,132 @@
+﻿using HarmonyLib;
+using ShinyShoe;
+using System.Collections.Generic;
+using System.Reflection;
+using Trainworks.Managers;
+using UnityEngine;
+
+namespace Trainworks.BuildersV2
+{
+    class SpChallengeDataBuilder
+    {
+        private string challengeID;
+
+        /// <summary>
+        /// Unique string used to store and retrieve the challenge data.
+        /// Implicitly sets NameKey and DescriptionKey.
+        /// </summary>
+        public string ChallengeID
+        {
+            get { return challengeID; }
+            set
+            {
+                challengeID = value;
+                if (NameKey == null)
+                {
+                    NameKey = challengeID + "_SpChallengeData_NameKey";
+                }
+                if (DescriptionKey == null)
+                {
+                    DescriptionKey = challengeID + "_SpChallengeData_DescriptionKey";
+                }
+            }
+        }
+        /// <summary>
+        /// Name displayed for the challenge.
+        /// Note that setting this property will set the localization for all languages.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Localization key for the challenge's name.
+        /// Note that setting challengeID sets this field to [ChallengeID]_SpChallengeData_NameKey.
+        /// </summary>
+        public string NameKey { get; set; }
+        /// <summary>
+        /// Description displayed for the challenge.
+        /// Note that setting this property will set the localization for all languages.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Localization key for the challenge's description.
+        /// Note that setting challengeID sets this field to [ChallengeID]_SpChallengeData_DescriptionKey.
+        /// </summary>
+        public string DescriptionKey { get; set; }
+
+        /// <summary>
+        /// List of Mutators to add to the ExpertChallenge to.
+        /// </summary>
+        public List<MutatorData> Mutators { get; set; }
+        /// <summary>
+        /// Convenience Builders for Mutators. Will be appended to Mutators.
+        /// </summary>
+        public List<MutatorDataBuilder> MutatorBuilders { get; set; }
+        /// <summary>
+        /// Require DLC.
+        /// </summary>
+        public DLC RequiredDLC { get; set; }
+        /// <summary>
+        /// The full, absolute path to the asset.
+        /// </summary>
+        public string FullAssetPath => BaseAssetPath + "/" + IconPath;
+        /// <summary>
+        /// Set automatically in the constructor. Base asset path, usually the plugin directory.
+        /// </summary>
+        public string BaseAssetPath { get; private set; }
+        /// <summary>
+        /// Custom asset path to load challenge art from. 76x76 image.
+        /// </summary>
+        public string IconPath { get; set; }
+
+        public SpChallengeDataBuilder()
+        {
+            Mutators = new List<MutatorData>();
+            MutatorBuilders = new List<MutatorDataBuilder>();
+            var assembly = Assembly.GetCallingAssembly();
+            BaseAssetPath = PluginManager.PluginGUIDToPath[PluginManager.AssemblyNameToPluginGUID[assembly.FullName]];
+        }
+
+        /// <summary>
+        /// Builds the RelicData represented by this builder's parameters
+        /// and registers it and its components with the appropriate managers.
+        /// </summary>
+        /// <returns>The newly registered SpChallengeData</returns>
+        public SpChallengeData BuildAndRegister()
+        {
+            var challengeData = Build();
+            CustomChallengeManager.RegisterCustomChallenge(challengeData);
+            return challengeData;
+        }
+
+        /// <summary>
+        /// Builds the RelicData represented by this builder's parameters
+        /// all Builders represented in this class's various fields will also be built.
+        /// </summary>
+        /// <returns>The newly created RelicData</returns>
+        public SpChallengeData Build()
+        {
+            var challengeData = ScriptableObject.CreateInstance<SpChallengeData>();
+            AccessTools.Field(typeof(GameData), "id").SetValue(challengeData, ChallengeID);
+            challengeData.name = ChallengeID;
+
+            var mutators = challengeData.GetMutators();
+            mutators.AddRange(Mutators);
+            foreach (var builder in MutatorBuilders)
+                mutators.Add(builder.Build());
+
+            // RelicData fields
+            AccessTools.Field(typeof(SpChallengeData), "nameKey").SetValue(challengeData, NameKey);
+            AccessTools.Field(typeof(SpChallengeData), "descriptionKey").SetValue(challengeData, DescriptionKey);
+            AccessTools.Field(typeof(SpChallengeData), "requiredDLC").SetValue(challengeData, RequiredDLC);
+            if (IconPath != null)
+            {
+                Sprite iconSprite = CustomAssetManager.LoadSpriteFromPath(FullAssetPath);
+                AccessTools.Field(typeof(SpChallengeData), "icon").SetValue(challengeData, iconSprite);
+            }
+
+            BuilderUtils.ImportStandardLocalization(DescriptionKey, Description);
+            BuilderUtils.ImportStandardLocalization(NameKey, Name);
+
+            return challengeData;
+        }
+    }
+}
---
+++ TrainworksModdingToolsExtended/Managers/CustomChallengeManager.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx.Logging;
+using System.Collections.Generic;
+using Trainworks.Utilities;
+
+namespace Trainworks.Managers
+{
+    class CustomChallengeManager
+    {
+        private static Dictionary<string, SpChallengeData> CustomChallengeData = new Dictionary<string, SpChallengeData>();
+
+        public static void RegisterCustomChallenge(SpChallengeData challengeData)
+        {
+            if (!CustomChallengeData.ContainsKey(challengeData.GetID()))
+            {
+                CustomChallengeData.Add(challengeData.GetID(), challengeData);
+                ProviderManager.SaveManager.GetAllGameData().GetBalanceData().GetSpChallenges().Add(challengeData);
+            }
+            else
+            {
+                Trainworks.Log(LogLevel.Warning, "Attempted to register duplicate challenge data with name: " + challengeData.name);
+            }
+        }
+
+        public static SpChallengeData FindChallengeByID(string id)
+        {
+            var guid = GUIDGenerator.GenerateDeterministicGUID(id);
+            if (CustomChallengeData.ContainsKey(guid))
+            {
+                return CustomChallengeData[guid];
+            }
+
+            foreach (var challenge in ProviderManager.SaveManager.GetBalanceData().GetSpChallenges())
+            {
+                if (challenge.GetID() == id)
+                {
+                    return challenge;
+                }
+            }
+
+            Trainworks.Log(LogLevel.All, "Couldn't find challenge: " + id + " - This will cause crashes.");
+            return null;
+
+        }
+    }
+}
