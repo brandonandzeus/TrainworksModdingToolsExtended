fbbcb0bb1c96d0f102981b7df2db4846362e9651
Brandon <brandon@brandonandzeus.com>
2023-04-10 16:13:45 -0700

    Added Divine Temple's Enhancer Pool IDs.

--- TrainworksModdingToolsExtended/Constants/VanillaEnhancerPoolIDs.cs
+++ TrainworksModdingToolsExtended/Constants/VanillaEnhancerPoolIDs.cs
@@ -10,7 +10,7 @@
         /// </summary>
         public static readonly string SpellUpgradePoolCostReduction = "SpellUpgradePoolCostReduction";
         /// <summary>
-        /// The second slot in the Spell Upgrade Shop. Contains Magic Power +10 and Consume, Magic Power +20. 
+        /// The second slot in the Spell Upgrade Shop. Contains Magic Power +10 and Consume, Magic Power +20 and Consume. 
         /// </summary>
         public static readonly string SpellUpgradePoolCommon = "SpellUpgradePoolCommon";
         /// <summary>
@@ -25,5 +25,13 @@
         /// The last slots in the Unit Upgrade Shop. Contains themed rare upgrades available for all classes. Multistrike (Hellhorned), Quick (Awoken), Largestone (Umbra) and Endless (Remnant). Stygian is in rare Spell upgrades.
         /// </summary>
         public static readonly string UnitUpgradePool = "UnitUpgradePool";
+        /// <summary>
+        /// The common slot in the Divine Temple. Intrinsic, + 10 and Pierce, and -1 Cost and Purge are here.
+        /// </summary>
+        public static readonly string SpellUpgradePoolDarkPactCommon = "SpellUpgradePoolDarkPactCommon";
+        /// <summary>
+        /// The uncommon slot in the Divine Temple. The +30 Magic Power, -2 Cost, and Spellchain are here. 
+        /// </summary>
+        public static readonly string SpellUpgradePoolDarkPactUncommon = "SpellUpgradePoolDarkPactUncommon";
     }
 }
