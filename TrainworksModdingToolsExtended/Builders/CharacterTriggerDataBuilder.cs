40ea4fdb8096e01dcf37b36e7edd4044aff1f5d7
Brandon <brandon@brandonandzeus.com>
2023-04-09 23:21:41 -0700

    Cleanup CharacterTriggerDataBuilder

--- TrainworksModdingToolsExtended/Builders/CharacterTriggerDataBuilder.cs
+++ TrainworksModdingToolsExtended/Builders/CharacterTriggerDataBuilder.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 
 namespace Trainworks.BuildersV2
 {
-    // Note code is unchanged from Trainworks Modding Tools.
     public class CharacterTriggerDataBuilder
     {
         private CharacterTriggerData.Trigger trigger;
-
         /// <summary>
         /// Implicitly sets DescriptionKey and AdditionalTextOnTriggerKey if null.
+        /// It is highly recommended to set DescriptionKey and AdditionalTextOnTriggerKey as they aren't unique
         /// </summary>
         public CharacterTriggerData.Trigger Trigger
         {
@@ -27,16 +26,14 @@ namespace Trainworks.BuildersV2
                 }
             }
         }
-
         /// <summary>
-        /// Append to this list to add new card effects. The Build() method recursively builds all nested builders.
+        /// Append to this list to add new card effects.
         /// </summary>
         public List<CardEffectDataBuilder> EffectBuilders { get; set; }
         /// <summary>
         /// List of pre-built card effects.
         /// </summary>
         public List<CardEffectData> Effects { get; set; }
-
         /// <summary>
         /// Description displayed for the relic.
         /// Note that setting this field will set the localization for all langauges.
@@ -47,7 +44,6 @@ namespace Trainworks.BuildersV2
         /// Note that setting this field will set the localization for all langauges.
         /// </summary>
         public string AdditionalTextOnTrigger { get; set; }
-
         /// <summary>
         /// Localization key for the text's localization.
         /// *HIGHLY* Recommended to set this as the description key is not a unique key.
@@ -58,7 +54,6 @@ namespace Trainworks.BuildersV2
         /// *HIGHLY* Recommended to set this as the localization key is not a unique key.
         /// </summary>
         public string AdditionalTextOnTriggerKey { get; set; }
-
         public bool DisplayEffectHintText { get; set; }
         public bool HideTriggerTooltip { get; set; }
 
@@ -66,28 +61,34 @@ namespace Trainworks.BuildersV2
         {
             EffectBuilders = new List<CardEffectDataBuilder>();
             Effects = new List<CardEffectData>();
+            HideTriggerTooltip = false;
+            DisplayEffectHintText = false;
         }
 
         /// <summary>
-        /// Builds the CharacterTriggerData represented by this builders's parameters recursively;
+        /// Builds the CharacterTriggerData represented by this builders's parameters
         /// all Builders represented in this class's various fields will also be built.
         /// </summary>
         /// <returns>The newly created CardTraitData</returns>
         public CharacterTriggerData Build()
         {
+            CharacterTriggerData characterTriggerData = new CharacterTriggerData(Trigger, /* cardEffectData = */null);
+
+            var effects = characterTriggerData.GetEffects();
+            effects.Clear(); // Contains a null from constructor above.
+            effects.AddRange(Effects);
             foreach (var builder in EffectBuilders)
-            {
-                Effects.Add(builder.Build());
-            }
-            CharacterTriggerData characterTriggerData = new CharacterTriggerData(Trigger, null);
-            BuilderUtils.ImportStandardLocalization(AdditionalTextOnTriggerKey, AdditionalTextOnTrigger);
+                effects.Add(builder.Build());
+
             AccessTools.Field(typeof(CharacterTriggerData), "additionalTextOnTriggerKey").SetValue(characterTriggerData, AdditionalTextOnTriggerKey);
-            BuilderUtils.ImportStandardLocalization(DescriptionKey, Description);
             AccessTools.Field(typeof(CharacterTriggerData), "descriptionKey").SetValue(characterTriggerData, DescriptionKey);
             AccessTools.Field(typeof(CharacterTriggerData), "displayEffectHintText").SetValue(characterTriggerData, DisplayEffectHintText);
-            AccessTools.Field(typeof(CharacterTriggerData), "effects").SetValue(characterTriggerData, Effects);
             AccessTools.Field(typeof(CharacterTriggerData), "hideTriggerTooltip").SetValue(characterTriggerData, HideTriggerTooltip);
             AccessTools.Field(typeof(CharacterTriggerData), "trigger").SetValue(characterTriggerData, Trigger);
+
+            BuilderUtils.ImportStandardLocalization(DescriptionKey, Description);
+            BuilderUtils.ImportStandardLocalization(AdditionalTextOnTriggerKey, AdditionalTextOnTrigger);
+
             return characterTriggerData;
         }
     }
