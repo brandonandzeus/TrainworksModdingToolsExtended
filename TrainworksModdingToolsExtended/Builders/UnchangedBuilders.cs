4b360282d99d782e0d44e6c3e0c65d5e268479cd
Brandon <brandon@brandonandzeus.com>
2023-04-10 23:17:55 -0700

    Remove UnchangedBuilders. Its better if I reimplement all of them.

--- TrainworksModdingToolsExtended/Builders/UnchangedBuilders.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Trainworks.BuildersV2
-{
-    // A hack for class name aliasing, nothing changed here in the DLC, but wanted to make
-    // usuability easier with a clean switch from Trainworks.Builders to Trainworks.BuildersV2.
-    class ChampionCardDataBuilder : Builders.ChampionCardDataBuilder { }
-    class CardUpgradeTreeDataBuilder : Builders.CardUpgradeTreeDataBuilder { }
-    class CardPoolBuilder : Builders.CardPoolBuilder { }
-    class EnhancerDataBuilder : Builders.EnhancerDataBuilder { }
-}
