f46181ebfb0bc512b3f27d56a17c23f8197c6003
Brandon <brandon@brandonandzeus.com>
2023-04-10 23:23:46 -0700

    Add CardPoolBuilder. I have no issues with this one so its an alias to Trainwork's version (plus the Manager isn't visible.)

---
+++ TrainworksModdingToolsExtended/Builders/CardPoolBuilder.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using Trainworks.Managers;
+using UnityEngine;
+
+namespace Trainworks.BuildersV2
+{
+    // A hack for class name aliasing, nothing changed here in the DLC, but wanted to make
+    // usuability easier with a clean switch from Trainworks.Builders to Trainworks.BuildersV2.
+    // No changes to this class, and OK in terms of its design.
+    public class CardPoolBuilder : Builders.CardPoolBuilder { }
+}
