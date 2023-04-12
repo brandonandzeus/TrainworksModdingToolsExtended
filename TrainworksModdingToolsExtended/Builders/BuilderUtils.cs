2215e7cee5eccbd4b5381b79be2399f1cf5b782c
Brandon <brandon@brandonandzeus.com>
2023-04-11 23:41:42 -0700

    Remove BuilderUtils.AddStatusEffect, low value inefficent function.
    Prevent empty localization key from being added.

--- TrainworksModdingToolsExtended/Builders/BuilderUtils.cs
+++ TrainworksModdingToolsExtended/Builders/BuilderUtils.cs
@@ -2,33 +2,8 @@
 
 namespace Trainworks.BuildersV2
 {
-    // Unchanged from Trainworks.
     public class BuilderUtils
     {
-        /// <summary>
-        /// Create a new status effect array and add the status effect with the specified information onto the end of it.
-        /// </summary>
-        /// <param name="statusEffectID">ID of the status effect</param>
-        /// <param name="stackCount">Number of stacks to apply</param>
-        /// <param name="oldStatuses">Status effect array to append to</param>
-        /// <returns>A new status effect array one element longer than the previous one, with the status effect in the last slot</returns>
-        public static StatusEffectStackData[] AddStatusEffect(string statusEffectID, int stackCount, StatusEffectStackData[] oldStatuses)
-        {
-            var statusEffectData = new StatusEffectStackData
-            {
-                statusId = statusEffectID,
-                count = stackCount
-            };
-            var newStatuses = new StatusEffectStackData[oldStatuses.Length + 1];
-            int i;
-            for (i = 0; i < oldStatuses.Length; i++)
-            {
-                newStatuses[i] = oldStatuses[i];
-            }
-            newStatuses[i] = statusEffectData;
-            return newStatuses;
-        }
-
         /// <summary>
         /// Imports localization data for a key.
         /// Sets the translation to text for all languages.
@@ -38,10 +13,9 @@ namespace Trainworks.BuildersV2
         /// <param name="text">Text for the key in all languages</param>
         public static void ImportStandardLocalization(string key, string text)
         {
-            if (key != null && text != null)
-            {
-                CustomLocalizationManager.ImportSingleLocalization(key, "Text", "", "", "", "", text, text, text, text, text, text);
-            }
+            if (key == "") return;
+            if (key == null || text == null) return;
+            CustomLocalizationManager.ImportSingleLocalization(key, "Text", "", "", "", "", text, text, text, text, text, text);
         }
     }
 }
