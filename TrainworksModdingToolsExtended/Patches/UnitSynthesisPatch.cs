489848488a33b335e8d63b8d1062b8de7432b9ad
Brandon <brandon@brandonandzeus.com>
2023-05-07 14:45:06 -0700

    Remove logging.

--- TrainworksModdingToolsExtended/Patches/UnitSynthesisPatch.cs
+++ TrainworksModdingToolsExtended/Patches/UnitSynthesisPatch.cs
@@ -11,10 +11,7 @@ namespace Trainworks.Patches
     {
         public static void Postfix(CharacterData characterData, ref CardUpgradeData __result)
         {
-            Trainworks.Log("" + characterData);
             var upgrade = CustomUpgradeManager.GetUpgradeData(characterData);
-            Trainworks.Log("" + upgrade);
-            Trainworks.Log("" + __result);
             if (upgrade != null)
             {
                 __result = upgrade;
