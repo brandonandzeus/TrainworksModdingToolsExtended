3d6a84f4b990687a0b72a36728ea31beffe99f57
Brandon <brandon@brandonandzeus.com>
2023-05-07 14:44:10 -0700

    Better Unit synthesis patch.

---
+++ TrainworksModdingToolsExtended/Patches/UnitSynthesisPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Trainworks.ManagersV2;
+
+namespace Trainworks.Patches
+{
+    [HarmonyPatch(typeof(UnitSynthesisMapping), nameof(UnitSynthesisMapping.GetUpgradeData))]
+    public class UnitSynthesisPatch
+    {
+        public static void Postfix(CharacterData characterData, ref CardUpgradeData __result)
+        {
+            Trainworks.Log("" + characterData);
+            var upgrade = CustomUpgradeManager.GetUpgradeData(characterData);
+            Trainworks.Log("" + upgrade);
+            Trainworks.Log("" + __result);
+            if (upgrade != null)
+            {
+                __result = upgrade;
+            }
+        }
+    }
+}
