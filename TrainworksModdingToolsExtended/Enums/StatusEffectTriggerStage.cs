ed7643a1fa1eda59916655755dcb034b75aa390a
Brandon <brandon@brandonandzeus.com>
2023-06-09 16:36:31 -0700

    Introduce ability to safely make new StatusEffectData.TriggerStage and TargetMode enums.

---
+++ TrainworksModdingToolsExtended/Enums/StatusEffectTriggerStage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainworks.Enums
+{
+    public class StatusEffectTriggerStage : ExtendedByteEnum<StatusEffectTriggerStage, StatusEffectData.TriggerStage>
+    {
+        private static byte InitialID = 128;
+
+        public StatusEffectTriggerStage(string Name, byte? ID = null) : base(Name, ID ?? GetNewGUID())
+        {
+
+        }
+
+        public static byte GetNewGUID()
+        {
+            InitialID++;
+            return InitialID;
+        }
+    }
+
+}
---
+++ TrainworksModdingToolsExtended/Enums/TargetModeType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trainworks.Enums;
+using static TargetMode;
+
+namespace Trainworks.Enums
+{
+    public class TargetModeType : ExtendedByteEnum<TargetModeType, TargetMode>
+    {
+        private static byte InitialID = 128;
+
+        public TargetModeType(string Name, byte? ID = null) : base(Name, ID ?? GetNewGUID())
+        {
+
+        }
+
+        public static byte GetNewGUID()
+        {
+            InitialID++;
+            return InitialID;
+        }
+    }
+}
