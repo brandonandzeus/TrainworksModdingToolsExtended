dbdf9a495cfa2bc810cdb1f381a2433d50a725c8
Brandon <brandon@brandonandzeus.com>
2023-06-09 14:12:24 -0700

    Add Utility Builder for ReplacementStrings.

---
+++ TrainworksModdingToolsExtended/Builders/ReplacementStringData.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainworks.BuildersV2
+{
+    class ReplacementStringDataBuilder
+    {
+        public string Keyword { get; set; }
+        public string ReplacementTextKey { get; set; }
+        public ReplacementStringData Build()
+        {
+            ReplacementStringData replacementStringData = new ReplacementStringData();
+            AccessTools.Field(typeof(ReplacementStringData), "_keyword").SetValue(replacementStringData, Keyword);
+            AccessTools.Field(typeof(ReplacementStringData), "_replacement").SetValue(replacementStringData, ReplacementTextKey);
+            return replacementStringData;
+        }
+    }
+}
